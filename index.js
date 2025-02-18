df1fc7a9075e0fdcefc61207cd1be55ddf7177a2
KeumKyuHwan <khkum93@naver.com>
2025-02-18 16:29:51 +0900

    자바스크립트 코드로 변경

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './App'; // App.js 파일을 불러옵니다
+import { name as appName } from './app.json'; // app.json에서 앱 이름을 불러옵니다
 
+// 앱의 루트 컴포넌트를 AppRegistry에 등록
 AppRegistry.registerComponent(appName, () => App);
+
