256259401ba7cd383a90a2ce3426b438a789ad85
suhyeon <tngus1633@gmail.com>
2025-02-21 18:34:09 +0900

    textStyle 업데이트

--- src/components/TextStyle.js
+++ src/components/TextStyle.js
@@ -1,15 +1,73 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text } from "react-native";
+import { Text, PixelRatio } from "react-native";
 
+const remToPx = (rem) => {
+    const baseFontSize = 16;
+    const scale = PixelRatio.get();
+    return rem * baseFontSize * scale;
+}
 
 const Font = styled(Text)`
-color : ${(props) => props.color || 'rgb(0,0,0)'};
+color : ${(props) => {
+    switch (props.color) {
+        case 'lightgray' :
+            return `rgb(207,207,207)`;
+        case 'darkgray' :
+            return `rgb(130,130,130)`;
+        case 'black' : 
+            return `rgb(0,0,0)`;
+        case 'white' :
+            return `rgb(255,255,255)`;
+        default :
+            return `rgb(0,0,0)`; // 기본값
+    }
+}
+};
+
+font-size: ${(props) => {
+    switch (props.size) {
+        case 'xsmall':
+            return `${remToPx(0.75)}px`; // 12px
+        case 'small' : 
+            return `${remToPx(1)}px`; // 16px
+        case 'medium' : 
+            return `${remToPx(1.25)}px`; // 20px
+        case 'large' :
+            return `${remToPx(1.5)}px`; // 24px
+        case 'xlarge' :
+            return `${remToPx(2)}px`; // 32px
+        default :
+            return `${remToPx(1.25)}px`; // 기본값
+    }
+}
+};
+
+font-weight : ${(props) => {
+    switch (props.weight) {
+        case 'normal' :
+            return 'normal' ;
+        case 'bold' :
+            return 'bold' ;
+        default:
+            return 'normal' ;
+    }
+}}
 
 `
 
-export const InfoFont = ({text, color}) => {
-    return <Font color={color}>{text}</Font>
-};
+function TextStyle ({text, color, size, weight}) {
+    return (
+        <Font 
+    color={color}
+    size={size}
+    weight={weight}
+    >
+        {text}
+    </Font>
+    )
+}
+
+export default TextStyle;
 
 // 밑에 사용하는 폰트 만들어서 사용할 것
