a6026fb48c1925247094fe63f00701c4c0c3d8e8
KeumKyuHwan <khkum93@naver.com>
2025-02-25 19:44:22 +0900

    TimeDropDown 수정

--- src/components/TimeDropDown.js
+++ src/components/TimeDropDown.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, Platform } from 'react-native';
+import { View, Text } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import Modal from 'react-native-modal';  // 모달을 사용하기 위한 import
 import styled from 'styled-components/native';
 
 const TimeWrapper = styled(View)`
@@ -18,12 +19,13 @@ const TimeText = styled(Text)`
 const TimeDropDown = () => {
   const [show, setShow] = useState(false); // 날짜/시간 선택창을 보여줄지 여부
   const [selectedTime, setSelectedTime] = useState(new Date()); // 선택된 시간
+  const [isModalVisible, setModalVisible] = useState(false); // 모달 창 상태
 
   const onChange = (event, selectedDate) => {
-    setShow(false); // 선택 후 선택창을 닫음
     if (selectedDate) {
       setSelectedTime(selectedDate); // 선택된 시간을 상태에 저장
     }
+    setModalVisible(false); // 모달 창 닫기
   };
 
   return (
@@ -31,20 +33,26 @@ const TimeDropDown = () => {
       <TimeWrapper>
         <TimeText>근무 시간</TimeText>
         <Text
-          onPress={() => setShow(true)}
+          onPress={() => setModalVisible(true)} // 클릭 시 모달 창 열기
         >
           {selectedTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
         </Text>
       </TimeWrapper>
 
-      {show && (
-        <DateTimePicker
-          value={selectedTime}
-          mode="time" // 시간과 분만 선택
-          display="default"
-          onChange={onChange}
-        />
-      )}
+      {/* 모달 창 */}
+      <Modal
+        isVisible={isModalVisible}
+        onBackdropPress={() => setModalVisible(false)} // 배경 클릭 시 모달 닫기
+      >
+        <View style={{ backgroundColor: 'white', padding: 20 }}>
+          <DateTimePicker
+            value={selectedTime}
+            mode="time" // 시간과 분만 선택
+            display="default"
+            onChange={onChange}
+          />
+        </View>
+      </Modal>
     </View>
   );
 };
