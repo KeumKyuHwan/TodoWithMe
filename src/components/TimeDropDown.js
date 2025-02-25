16bb5207486402bac6f20e7297507010b808ac5c
KeumKyuHwan <khkum93@naver.com>
2025-02-25 16:18:25 +0900

    dropdown 추가

---
+++ src/components/TimeDropDown.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+  
+      // 시간과 10분 단위로 드롭다운 메뉴 항목 생성
+      const items = [];
+      for (let hour = 0; hour < 24; hour++) {
+        for (let minute = 0; minute < 60; minute += 10) {
+          const time = `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`;
+          items.push({ label: time, value: time });
+        }
+      }
+  
+      this.state = {
+        open: false,
+        value: [],
+        items: items,  // 시간과 10분 단위로 항목을 설정
+      };
+  
+      this.setValue = this.setValue.bind(this);
+    }
+  
+    setOpen(open) {
+      this.setState({
+        open,
+      });
+    }
+  
+    setValue(callback) {
+      this.setState((state) => ({
+        value: callback(state.value),
+      }));
+    }
+  
+    setItems(callback) {
+      this.setState((state) => ({
+        items: callback(state.items),
+      }));
+    }
+  
+    render() {
+      const { open, value, items } = this.state;
+  
+      return (
+        <DropDownPicker
+          multiple={true}
+          min={0}
+          max={20}
+          open={open}
+          value={value}
+          items={items}
+          setOpen={(open) => this.setOpen(open)}
+          setValue={(callback) => this.setValue(callback)}
+          setItems={(callback) => this.setItems(callback)}
+          placeholder="Select time"
+        />
+      );
+    }
+  }
+  
+  export default App;
